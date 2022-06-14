8873b6c7b22f85d754112e7d8fcca9e69be5a3b8
Nagito <c0a2103668@edu.teu.ac.jp>
2022-06-14 16:38:08 +0900

    変数を修正しました。 #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,19 +1,21 @@
 import random
 
+from pyparsing import quoted_string, quotedString
+
 def main():
     collect=shutsudai()
     kaitou(collect)
 
 def shutsudai():
-    allq=[
+    qq=[
         {"q":"サザエの旦那の名前は？","a":["ますお","マスオ"]},
         {"q":"カツオの妹の名前は？","a":["わかめ","ワカメ"]},
         {"q":"タラオはカツオから見てどんな関係？","a":["甥","おい","甥っ子","おいっこ"]}
     ]
     rq=random.randint(0,2)
     print("問題:")
-    print(allq[rq]["q"])
-    return allq[rq]["a"]
+    print(qq[rq]["q"])
+    return qq[rq]["a"]
 
 def kaitou(collect):
     ans=input("答えるんだ:")
