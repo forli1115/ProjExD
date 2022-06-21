54b7237cb44d0f2f94e7c9d4f64e59b7645e59d4
Nagito <c0a2103668@edu.teu.ac.jp>
2022-06-21 15:33:56 +0900

    電卓３限

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,35 +1,40 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-#ボタンが左クリックされたときに実行される
+
+#ボタンがクリックされたときに実行される関数
 def button_click(event):
     btn=event.widget
-    num=btn["text"]
-    #tkm.showinfo("",f"{num}のボタンがクリックされました")    
-    entry.insert(tk.END,num)
+    num=btn["text"]#クリックされたボタンの文字
+    if num=="=":
+        eqn=entry.get()
+        res=eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+    else:
+        #tkm.showinfo("",f"{num}のボタンがクリックされました")    
+        entry.insert(tk.END,num)
 
 
 if __name__=="__main__":
     root=tk.Tk()
-    root.title("超高機能電卓")
-    
-    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
-    entry.grid(row=0,column=0,columnspan=3)
-
+    root.title("電卓")
+    entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))#文字盤の作成
+    entry.grid(row=0,column=0,columnspan=3)#横方向に3マス結合
 
     r,c=1,0   #r:行番号　c:列番号
-    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
+    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
         btn = tk.Button(root,
                         text=num,
                         width=4,
                         height=2,
-                        font=("Times New Roman", 30))
+                        font=("Times New Roman", 30)
+                        )
 
-
-        btn.bind("<1>",button_click)
+        btn.bind("<1>",button_click)#<1>は左クリック
         btn.grid(row=r,column=c)
         c+=1
-        if (i+1)%3==0:
+        if (i+1)%3==0:#3マスおきに改行
             r+=1
             c=0
     
