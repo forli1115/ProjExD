3f1152d2f72a5c1a712bbe360c87975af5a4ab3d
Nagito <c0a2103668@edu.teu.ac.jp>
2022-06-21 14:54:41 +0900

    電卓４,５

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
+#ボタンが左クリックされたときに実行される
 def button_click(event):
     btn=event.widget
     num=btn["text"]
-    tkm.showinfo("",f"{num}のボタンがクリックされました")    
+    #tkm.showinfo("",f"{num}のボタンがクリックされました")    
+    entry.insert(tk.END,num)
 
 
 if __name__=="__main__":
@@ -15,17 +16,20 @@ if __name__=="__main__":
     entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman", 40))
     entry.grid(row=0,column=0,columnspan=3)
 
-    r,c=1,0
-    for num in range(9,-1,-1):
+
+    r,c=1,0   #r:行番号　c:列番号
+    for i,num in enumerate([i for i in range(9,-1,-1)]+["+"]):
         btn = tk.Button(root,
                         text=num,
                         width=4,
                         height=2,
                         font=("Times New Roman", 30))
+
+
         btn.bind("<1>",button_click)
         btn.grid(row=r,column=c)
         c+=1
-        if (num-1)%3==0:
+        if (i+1)%3==0:
             r+=1
             c=0
     
