114b1f09f8247fe5c40272021edcca1549c1bbce
Nagito <c0a2103668@edu.teu.ac.jp>
2022-06-28 14:38:12 +0900

    練習６

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,8 +3,11 @@ import tkinter as tk
 def key_down(event):
     global key
     key = event.keysym
-    print(f"{key}キーが押されました")
+    #print(f"{key}キーが押されました")
 
+def key_up(event):
+    global key
+    key = ""
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -20,5 +23,6 @@ if __name__ == "__main__":
     key = ""
 
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
 
     root.mainloop()
