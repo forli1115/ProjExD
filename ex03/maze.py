38bc4fec86a0ee99e8d7b352ad1daca5c9e68374
Nagito <c0a2103668@edu.teu.ac.jp>
2022-06-28 14:26:47 +0900

    練習３

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,11 +1,16 @@
 import tkinter as tk
 
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
     canvas = tk .Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
+
+    bird = tk.PhotoImage(file="fig/0.png")
+    cx, cy =300, 400
+    canvas.create_image(cx, cy, image=bird, tag="bird")
     
     root.mainloop()
