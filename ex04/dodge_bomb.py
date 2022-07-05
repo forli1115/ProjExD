52f41cb2ab1aa4db6113d48142e3ab2d3a7c4124
Nagito <c0a2103668@edu.teu.ac.jp>
2022-07-05 14:24:51 +0900

    練習３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,6 +3,7 @@ import sys
 
 
 def main():
+    #練習１
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ!こうかとん")
     screen_sfc = pg.display.set_mode((1600,900)) #Serface
@@ -11,13 +12,18 @@ def main():
     bgimg_rct = bgimg_sfc.get_rect()             #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
     
-    
+    #練習３
+    kkimg_sfc = pg.image.load("fig/9.png")       #Surface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0) #Surface
+    kkimg_rct = kkimg_sfc.get_rect()             #Rect
+    kkimg_rct.center = 900, 400
+
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
-
-        #練習2
+        #練習２
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
