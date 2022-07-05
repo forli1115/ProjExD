39ba2f41e8087ca49a87b2b997a005126cbfabc0
Nagito <c0a2103668@edu.teu.ac.jp>
2022-07-05 14:59:31 +0900

    練習5,6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import sys
-
+import random
 
 def main():
     #練習１
@@ -18,9 +18,20 @@ def main():
     kkimg_rct = kkimg_sfc.get_rect()             #Rect
     kkimg_rct.center = 900, 400
 
+    
+    #練習５:爆弾
+    bmimg_sfc = pg.Surface((20, 20)) #Surface
+    bmimg_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
+    bmimg_rct = bmimg_sfc.get_rect() #Rect
+    bmimg_rct.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct.centery = random.randint(0, screen_rct.height)
+    #練習６
+    vx, vy = +1, +1
 
     while True:
-        screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(bgimg_sfc, bgimg_rct) #screen Surfaceにbgimg_sfc Surfaceをbgimg_rctに従って張り付ける
+
         #練習２
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -32,8 +43,13 @@ def main():
         if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
         if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
         if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
-    
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct) #screen Surfaceにkkimg_sfc Surfaceをkkimg_rctに従って張り付ける
+
+        #練習５
+        screen_sfc.blit(bmimg_sfc, bmimg_rct) #screen Surfaceにbmimg_sfc Surfaceをbmimg_rctに従って張り付ける
+
+        #練習６
+        bmimg_rct.move_ip(vx, vy)
 
 
         pg.display.update() #画面を更新する
